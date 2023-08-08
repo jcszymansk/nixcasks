df54d2fd794c18c90d2f4daa3ced77728900617b
jacek szymanski <jacek.p.szymanski@gmail.com>
2023-08-08 13:46:53 +0200

    feat(7zip): try to support arm64 for 7zip

--- 7zip/default.nix
+++ 7zip/default.nix
@@ -1,10 +1,11 @@
 { stdenv
 , fetchurl
 , gnumake
+, system
 , ...
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   pname = "7zip";
   version = "23.01";
 
@@ -12,6 +13,11 @@ stdenv.mkDerivation {
     url = "https://www.7-zip.org/a/7z2301-src.tar.xz";
     sha256 = "0x4zpzjfnn0ggjprz30fawdvb014icz9j14r9n1a3rb0fc072q1m";
   };
+
+  arch = {
+    "x86_64-darwin" = "x64";
+    "aarch64-darwin" = "arm64";
+  }.${system} or (throw "unknown system ${system}");
 
   sourceRoot = ".";
 
@@ -26,12 +32,12 @@ stdenv.mkDerivation {
   buildPhase = ''
     export MACOSX_DEPLOYMENT_TARGET=11.0
     cd CPP/7zip/Bundles/Alone2
-    make -f ../../cmpl_mac_x64.mak
+    make -f ../../cmpl_mac_${arch}.mak
   '';
 
   installPhase = ''
     mkdir -p $out/bin
-    cp b/m_x64/7zz $out/bin
+    cp b/m_${arch}/7zz $out/bin
   '';
 
 }
