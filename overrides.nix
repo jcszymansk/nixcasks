7e7d67fae1c75c57f1659fcf59721719f2e56770
jacek szymanski <jacek.p.szymanski@gmail.com>
2023-11-27 18:11:41 +0100

    chore: mark little-snitch as broken (see issue #3)

--- overrides.nix
+++ overrides.nix
@@ -2,8 +2,10 @@ _:
 
 let
   noSandbox = _: { __noChroot = true; };
+  broken = _: { meta.broken = true; };
 in
 {
   macpass = _: { setSourceRoot = "sourceRoot=."; };
   docker = noSandbox;
+  little-snitch = broken;
 }
